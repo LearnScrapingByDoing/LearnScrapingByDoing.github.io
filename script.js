28091a3f991281369e82c48804a00303a46f5560
nate-moo <40650681+nate-moo@users.noreply.github.com>
2022-01-24 22:43:47 -0500

    some cleanup and additions

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 function openNav() {
     var sidenav = document.getElementById("sidenav")
-    let mainContent = document.getElementById("mainContent")
-
-    if (sidenav.style.width == "250px") {
-        mainContent.classList.remove("dark");
-        sidenav.style.width = "0px";
-    } else {
-        sidenav.style.width = "250px";
-        mainContent.classList.add("dark");
-    }
+    document.getElementById("mainContent").classList.toggle("dark");
+    sidenav.style.width == "250px" 
+        ? sidenav.style.width = "0px" 
+        : sidenav.style.width = "250px";
 }
