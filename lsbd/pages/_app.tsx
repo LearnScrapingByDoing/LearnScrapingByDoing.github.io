3d9a0b8ddff028f831ba42353a1290cd3b3cea78
nate-moo <40650681+nate-moo@users.noreply.github.com>
2022-04-22 12:15:17 -0400

    Starting stuff lol

--- lsbd/pages/_app.tsx
+++ lsbd/pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+
+  )
 }
 
 export default MyApp
